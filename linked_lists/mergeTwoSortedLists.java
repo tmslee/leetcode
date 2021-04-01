57f4606add7b2ad290b228b798f1c6a604a3eced
Sang Hyup Lee <tms.lee9@gmail.com>
2021-04-01 18:12:15 -0400

    finish merge two sorted Lists

--- linked_lists/mergeTwoSortedLists.java
+++ linked_lists/mergeTwoSortedLists.java
@@ -1,10 +1,25 @@
 package linked_lists;
 
 public class mergeTwoSortedLists {
-  public static solution () {
+  public static ListNode solution (ListNode l1, ListNode l2) {
+    if (l1 == null) return l2;
+    else if (l2 == null) return l1;
     
-  }
-
-  public static void main(String[] args){
+    ListNode ans = new ListNode();
+    ListNode ansPtr = ans;
+    
+    while (l1 != null && l2 != null) {
+        if(l1.val <= l2.val) {
+            ans.next = l1;
+            l1 = l1.next;
+        } else {
+            ans.next = l2;
+            l2 = l2.next;
+        }
+        ans = ans.next;
+    }
+    if(l1 != null) ans.next = l1;
+    else if(l2 != null) ans.next = l2;
+    return ansPtr.next;
   }
 }
