7236e9b964f34be02a7ef19326454dc6f0a5f88c
Sang Hyup Lee <tms.lee9@gmail.com>
2021-04-01 20:51:35 -0400

    finished copy list with random pointer

--- linked_lists/copyListWIthRandomPtr.java
+++ linked_lists/copyListWIthRandomPtr.java
@@ -1,10 +1,34 @@
 package linked_lists;
 
 public class copyListWIthRandomPtr {
-  public static solution () {
-    
-  }
+  public static Node solution (Node head) {
+    Node ansPtr = new Node(0);
+    Node currNode = ansPtr;
+    while (head != null) {
+        Node newNode = new Node(head.val);
+        newNode.random = head.random;
+        
+        Node tempNext = head.next;
+        head.next = newNode;
+        head = tempNext;
 
-  public static void main(String[] args){
+        currNode.next = newNode;
+        currNode = currNode.next;
+    }
+    
+    currNode = ansPtr.next;
+    while(currNode != null){
+        System.out.println("curr val is: " + currNode.val);
+        if(currNode.random != null) {
+            // System.out.println(currNode.random.val);
+            // System.out.println(currNode.random.next.val);
+            currNode.random = currNode.random.next;
+            System.out.println("random val is: " + currNode.random.val);
+        } else {
+            System.out.println("random val is: null");
+        }
+        currNode = currNode.next;
+    }
+    return ansPtr.next;
   }
 }
