a3d88c87b073aced8f2c03c7fb137ed4aa121caa
Sang Hyup Lee <tms.lee9@gmail.com>
2021-04-01 21:09:14 -0400

    finished reverse linkedlist

--- linked_lists/reverseLinkedList.java
+++ linked_lists/reverseLinkedList.java
@@ -1,9 +1,28 @@
 package linked_lists;
 
 public class reverseLinkedList {
-  public static solution () {
-    
+  public static ListNode solutionIter (ListNode head) {
+    ListNode prev = null;
+        
+    while(head != null){
+        ListNode temp = head.next;
+        head.next = prev;
+        prev = head; 
+        head = temp;
+    }
+    return prev;
   }
 
-  public static void main(String[] args){
+  public static ListNode solutionRecur (ListNode head) {
+    if (head == null) return head;
+    if (head.next == null) return head;
+    ListNode subReverse = solutionRecur(head.next);
+    ListNode ansPtr = subReverse;
+    
+    ListNode tempNext = head.next;
+    head.next = null;
+    
+    tempNext.next = head;
+    return ansPtr;
   }
+}
