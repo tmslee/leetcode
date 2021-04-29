9f6d4f04239c85f78d19edc4b7ba6cc9282c4e04
Sang Hyup Lee <tms.lee9@gmail.com>
2021-04-29 15:22:47 -0400

    search rotated sorted array

---
+++ sort_search/searchRotatedSortedArray.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int search(int[] nums, int target) {
+      int lo = 0;
+      int hi = nums.length - 1;
+      while (lo <= hi) {
+          int mid = lo + (hi - lo) / 2;
+          if (nums[mid] == target) return mid;
+          else if (nums[lo] <= nums[mid]){ //left side sorted
+              if(target >= nums[lo] && target < nums[mid]) hi = mid - 1;
+              else lo = mid + 1;
+          } else { //right side sorted
+              if(target > nums[mid] && target <= nums[hi]) lo = mid + 1;
+              else hi = mid - 1;
+          }
+      }
+      return -1;
+  }
+}
