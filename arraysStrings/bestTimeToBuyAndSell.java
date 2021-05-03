4d607fb9172d9d75334f79d4bb190b800e15e657
Sang Hyup Lee <tms.lee9@gmail.com>
2021-05-03 01:18:07 -0400

    best time to buy and sell

---
+++ arraysStrings/bestTimeToBuyAndSell.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int maxProfit(int[] prices) {
+      int ans = 0;
+      int min = Integer.MAX_VALUE;
+      
+      for (int i=0 ; i<prices.length ; i++) {
+          min = Math.min(prices[i], min);
+          ans = Math.max(ans, prices[i]-min);
+      }
+      return ans;
+  }
+}
