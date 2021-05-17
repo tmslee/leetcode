a9c965256366f48663b0d96edc1405d1ec8ed53f
Sang Hyup Lee <tms.lee9@gmail.com>
2021-05-16 21:48:45 -0400

    clone graph

---
+++ trees_graphs/cloneGraph.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+  Map<Node, Node> seen = new HashMap();
+  
+  public Node cloneGraph(Node node) {
+      if(node == null) return node;
+      
+      if(seen.containsKey(node)) return seen.get(node);
+      
+      Node clone = new Node(node.val, new ArrayList<Node>());
+      seen.put(node, clone);
+      
+      for(Node neighbor : node.neighbors) {
+          clone.neighbors.add(cloneGraph(neighbor));
+      }
+      
+      return clone;
+  }
+}
