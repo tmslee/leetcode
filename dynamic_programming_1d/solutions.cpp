93bbc8ccf6b1a195437f289f8eed651da0e8d8e5
Thomas <tms.lee9@gmail.com>
2026-02-24 15:05:21 -0500

    longest palindromic substring

---
+++ dynamic_programming_1d/solutions.cpp
@@ -0,0 +1,29 @@
+// longest palindromic substring
+std::string longestPalindrome(const std::string& s) {
+    if(s.empty()) return {};
+
+    using P = std::pair<int,int>;
+    const int len = std::ssize(s);
+    
+    auto expand = [&](int l, int r) -> P {
+        while(l>=0 && r<len && s[l]==s[r]) {
+            --l;
+            ++r;
+        }
+        return {l+1, r-1};
+    };
+
+    int best_start = 0;
+    int best_len = 0;
+    
+    for(int i=0; i<len; ++i) {
+        for(auto [l,r] : {expand(i,i), expand(i,i+1)}) {
+            const int length = r-l+1;
+            if(length > best_len) {
+                best_start = l;
+                best_len = length;
+            }
+        }
+    }
+    return s.substr(best_start, best_len);
+}
