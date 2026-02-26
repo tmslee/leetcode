01f174f732624491002013046960049f661596a5
Thomas <tms.lee9@gmail.com>
2026-02-26 13:24:03 -0500

    minimum window substring

--- sliding_window/solutions.cpp
+++ sliding_window/solutions.cpp
@@ -15,3 +15,38 @@ int lengthOfLongestSubstring(const std::string& s) {
     }
     return ans;
 }
+
+// 76 minimum window substring
+int getIdx(const char c) const {
+    return std::islower(c) ? c-'a' : c-'A' + 26;
+}
+
+std::string minWindow(const std::string& s, const std::string& t) {
+    const int len = std::ssize(s); 
+    std::vector<int> counts(52, 0);
+    int diff = 0;
+    
+    for(const char c : t) {
+        if(++counts[getIdx(c)] == 1) ++diff;
+    }
+
+    int start = 0;
+    int shortest_len = std::numeric_limits<int>::max();
+    int l=0;
+
+    for(int r=0; r<len; ++r) {
+        const int ridx = getIdx(s[r]);
+        if(--counts[ridx] == 0) --diff;
+
+        while(diff==0) {
+            if(r-l+1 < shortest_len) {
+                shortest_len = r-l+1;
+                start = l;
+            }
+            const int lidx = getIdx(s[l]);
+            if(++counts[lidx] == 1) ++diff;
+            ++l;
+        }
+    }
+    return (shortest_len == std::numeric_limits<int>::max()) ? "" : s.substr(start, shortest_len);
+}
