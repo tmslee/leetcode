8d4998866a04eb90a461c051fe717dfa3382d471
Thomas <tms.lee9@gmail.com>
2026-02-26 11:00:22 -0500

    same tree

--- trees/solutions.cpp
+++ trees/solutions.cpp
@@ -9,3 +9,13 @@ bool helper(const TreeNode* node, std::optional<int> lo, std::optional<int> hi) 
 bool isValidBST(const TreeNode* root) {
     return helper(root, std::nullopt, std::nullopt);
 }
+
+// 100 same tree
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(!p && !q) return true;
+    if(!p || !q) return false;
+    if(p->val == q->val) {
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+    return false;
+}
