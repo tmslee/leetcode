8f90eb81858ce3870fdcd659d8621a0426ed8bb4
Thomas <tms.lee9@gmail.com>
2026-02-26 11:51:07 -0500

    3sum

--- two_pointer/solutions.cpp
+++ two_pointer/solutions.cpp
@@ -13,3 +13,32 @@ int maxArea(const vector<int>& height) {
     }
     return ans;
 }
+
+// 15 3sum
+std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+    const int n = std::ssize(nums);
+    std::vector<std::vector<int>> ans;
+
+    for(int i=0; i<n; ++i) {
+        if(nums[i] > 0) break;
+        if(i>0 && nums[i] == nums[i-1]) continue;
+
+        int l = i+1;
+        int r = n-1;
+        while(l < r) {
+            const int tot = nums[l] + nums[r] + nums[i];
+            if(tot == 0) {
+                ans.push_back({nums[i], nums[l], nums[r]});
+            }
+            if(tot <= 0) {
+                ++l;
+                while(l<r && nums[l] == nums[l-1]) ++l;
+            } else {
+                --r;
+                while(l<r && nums[r] == nums[r+1]) --r;
+            }
+        }
+    }
+    return ans;
+}
