73de0440ef72367db104eb48c49c3fce7104cbe4
Thomas <tms.lee9@gmail.com>
2026-02-26 12:04:04 -0500

    first missing positive

--- arrays_hash/solutions.cpp
+++ arrays_hash/solutions.cpp
@@ -11,3 +11,25 @@ vector<int> twoSum(const vector<int>& nums, int target) {
     throw std::invalid_argument{"no two-sum solution exists"};
 }
 
+// 41 first missing positive
+int firstMissingPositive(vector<int>& nums) {
+    const int n = std::ssize(nums);
+    bool one_present = false;
+    for(int i=0; i<n; ++i) {
+        if(nums[i] == 1) one_present = true;
+        if(nums[i] <= 0) nums[i] = 1;
+    }
+    if(!one_present) return 1;
+    
+    for(int i=0; i<n; ++i) {
+        const int idx = std::abs(nums[i])-1;
+        if(idx < n) {
+            nums[idx] = -std::abs(nums[idx]);
+        }
+    }
+    
+    for(int i=0; i<n; ++i){
+        if(nums[i] > 0) return i+1;
+    }
+    return n+1;
+}
