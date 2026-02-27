b8a2fae8bf27dff6aff0e575c49e1ced35b2ce55
Thomas <tms.lee9@gmail.com>
2026-02-27 11:05:03 -0500

    generate parentheses

--- backtrack_recur/solutions.cpp
+++ backtrack_recur/solutions.cpp
@@ -24,3 +24,27 @@ std::vector<std::string> letterCombinations(const std::string& digits) {
     helper(0);
     return ans;
 }
+
+// 22 generate parentheses
+std::vector<string> generateParenthesis(const int n) {
+    std::vector<string> ans;
+    std::string curr;
+    auto gen = [&](this auto& self, const int opens, const int closes)-> void {
+        if(static_cast<int>(curr.size()) == n*2) {
+            ans.push_back(curr);
+            return;
+        } 
+        if(opens < n) {
+            curr.push_back('(');
+            self(opens+1, closes);
+            curr.pop_back();
+        }
+        if(closes < opens) {
+            curr.push_back(')');
+            self(opens, closes+1);
+            curr.pop_back();
+        }
+    };
+    gen(0, 0);
+    return ans;
+}
