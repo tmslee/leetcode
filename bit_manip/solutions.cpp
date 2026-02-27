605eb9f7e15c76f5e9c9a75285e42e5769b405a9
Thomas <tms.lee9@gmail.com>
2026-02-27 15:04:26 -0500

    reverse bits

--- bit_manip/solutions.cpp
+++ bit_manip/solutions.cpp
@@ -6,3 +6,14 @@ int singleNumber(const std::vector<int>& nums) {
     }
     return ans;
 }
+
+// 190 reverse bits
+uint32_t reverseBits(uint32_t n) {
+    uint32_t ans = 0;
+    for(int i=0; i<32; ++i) {
+        ans <<= 1;
+        ans |= (n&1);
+        n >>= 1;
+    }
+    return ans;
+}
