fed59cd14a2fb4a8517508bdddc8bb979eb26ed3
Thomas <tms.lee9@gmail.com>
2026-03-03 09:47:31 -0500

    merge two sorted lists

--- linked_list/solutions.cpp
+++ linked_list/solutions.cpp
@@ -35,3 +35,21 @@ ListNode* removeNthFromEnd(ListNode* head, const int n) {
 
     return dummy.next;
 }
+
+// 21 merge two sorted lists
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode dummy(0);
+    ListNode* curr = &dummy;
+    while(list1 && list2) {
+        if(list1->val < list2->val) {
+            curr->next = list1;
+            list1 = list1->next;
+        } else {
+            curr->next = list2;
+            list2 = list2->next;
+        }
+        curr = curr->next;
+    }
+    curr->next = list1 ? list1 : list2;
+    return dummy.next;
+}
