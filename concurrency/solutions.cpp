e82a5529c9c50f281396165f0a632c2b37f755aa
Thomas <tms.lee9@gmail.com>
2026-03-03 15:23:57 -0500

    print foobar alternatively

--- concurrency/solutions.cpp
+++ concurrency/solutions.cpp
@@ -99,3 +99,96 @@ public:
         printThird();
     }
 };
+
+// 1115 print foobar alternatively
+class FooBar {
+private:
+    const int n_;
+    int curr_ = 1;
+    std::mutex m_;
+    std::condition_variable cv_;
+    bool foo_turn = true;
+
+public:
+    explicit FooBar(int n) : n_(n) {}
+
+    void foo(function<void()> printFoo) {
+        while(true) {
+            std::unique_lock<std::mutex> lk(m_);
+            cv_.wait(lk, [this]{ return curr_ > n_ || foo_turn; });
+            if(curr_ > n_) {
+                cv_.notify_one();
+                break;
+            }
+            printFoo();
+            foo_turn = false;
+            cv_.notify_one();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        while(true) {
+            std::unique_lock<std::mutex> lk(m_);
+            cv_.wait(lk, [this]{ return curr_ > n_ || !foo_turn; });
+            if(curr_ > n_) {
+                cv_.notify_one();
+                break;
+            }
+            printBar();
+            ++curr_;
+            foo_turn = true;
+            cv_.notify_one();
+        }
+    }
+};
+// semaphore solution
+class FooBar {
+    const int n_;
+    std::binary_semaphore foo_sem_{1};  // foo goes first
+    std::binary_semaphore bar_sem_{0};
+
+public:
+    explicit FooBar(int n) : n_(n) {}
+
+    void foo(std::function<void()> printFoo) {
+        for (int i = 0; i < n_; ++i) {
+            foo_sem_.acquire();
+            printFoo();
+            bar_sem_.release();
+        }
+    }
+
+    void bar(std::function<void()> printBar) {
+        for (int i = 0; i < n_; ++i) {
+            bar_sem_.acquire();
+            printBar();
+            foo_sem_.release();
+        }
+    }
+};
+// atomics solution
+class FooBar {
+    const int n_;
+    std::atomic<bool> foo_turn_{true};
+
+public:
+    explicit FooBar(int n) : n_(n) {}
+
+    void foo(std::function<void()> printFoo) {
+        for (int i = 0; i < n_; ++i) {
+            foo_turn_.wait(false, std::memory_order_acquire);  // block while false
+            printFoo();
+            foo_turn_.store(false, std::memory_order_release);
+            foo_turn_.notify_one();
+        }
+    }
+
+    void bar(std::function<void()> printBar) {
+        for (int i = 0; i < n_; ++i) {
+            foo_turn_.wait(true, std::memory_order_acquire);  // block while true
+            printBar();
+            foo_turn_.store(true, std::memory_order_release);
+            foo_turn_.notify_one();
+        }
+    }
+};
